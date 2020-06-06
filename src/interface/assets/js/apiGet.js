f7a445e3092e47982bac5bf7640123139a90550c
Diego Lasarin <lasarindiego@gmail.com>
2020-06-06 16:36:29 -0300

    Ajustes nas APIs e proteções contra erros nos retornos.

--- src/interface/assets/js/apiGet.js
+++ src/interface/assets/js/apiGet.js
@@ -38,39 +38,67 @@ function getApi(request){
         document.getElementById('sucessoPlino').innerHTML = response.data.plino.status == 200 ? 'Sim' : 'Não';
         document.getElementById('plinoTb').innerHTML = response.data.plino.email_class;
 
-      /*  //Antideo
+        //Antideo
             //ipHealth
-            document.getElementById('proxy').innerHTML = 'Proxy: ' + response.data.ipHealth.proxy;
-            document.getElementById('spam').innerHTML = 'SPAM: ' + response.data.ipHealth.spam;
-            document.getElementById('toxic').innerHTML = 'Tóxico: ' + response.data.ipHealth.toxic;
+            if(response.data.hasOwnProperty('ipHealth')){
+                document.getElementById('proxy').innerHTML = response.data.ipHealth.hasOwnProperty('proxy') ? 'Proxy: ' +  response.data.ipHealth.proxy : 'Não foi possível verificar o Proxy';
+                document.getElementById('spam').innerHTML = response.data.ipHealth.hasOwnProperty('spam') ? 'SPAM: ' + response.data.ipHealth.spam : 'Não foi possível verificar se é SPAM';
+            } else {
+                document.getElementById('proxy').innerHTML = 'Não foi possível verificar o Proxy';
+                document.getElementById('spam').innerHTML = 'Não foi possível verificar se é SPAM';
+            }
 
             //ipInfo
-            document.getElementById('host').innerHTML = 'Host: ' + response.data.ipInfo.host;
-            document.getElementById('ip').innerHTML = 'IP: ' + response.data.ipInfo.IP;
-            document.getElementById('category').innerHTML = 'Categoria: ' + response.data.ipInfo.org.category;
-            document.getElementById('company').innerHTML = 'Empresa: ' + response.data.ipInfo.org.name;
+            if(response.data.hasOwnProperty('ipInfo')){
+                document.getElementById('host').innerHTML = response.data.ipInfo.hasOwnProperty('host') ? 'Host: ' + response.data.ipInfo.host : 'Não foi possível encontrar o host';
+                document.getElementById('ip').innerHTML = response.data.ipInfo.hasOwnProperty('IP') ? 'IP: ' +  response.data.ipInfo.IP : 'Não foi possível encontrar o IP';
+                document.getElementById('category').innerHTML = response.data.ipInfo.hasOwnProperty('org') && response.data.ipInfo.org.hasOwnProperty('category')  ? 'Categoria: ' + response.data.ipInfo.org.category : 'Não foi possível encontrar nenhuma categoria';
+                document.getElementById('company').innerHTML = response.data.ipInfo.hasOwnProperty('org') && response.data.ipInfo.org.hasOwnProperty('name') ? 'Empresa: ' + response.data.ipInfo.org.name : 'Não foi possível encontrar a empresa';
+            }  else {
+                document.getElementById('host').innerHTML = 'Não foi possível encontrar o host';
+                document.getElementById('ip').innerHTML = 'Não foi possível encontrar o IP';
+                document.getElementById('category').innerHTML = 'Não foi possível encontrar nenhuma categoria';
+                document.getElementById('company').innerHTML = 'Não foi possível encontrar a empresa';
+            }
 
             //ipLocation
-            document.getElementById('city').innerHTML = 'Cidade: ' + response.data.ipLocation.city;
-            document.getElementById('country').innerHTML = 'País: ' + response.data.ipLocation.country;
-            document.getElementById('countrycode').innerHTML = 'Código do país: ' + response.data.ipLocation.country_code;
-            document.getElementById('latitude').innerHTML = 'Latitude: ' + response.data.ipLocation.latitude;
-            document.getElementById('longitude').innerHTML = 'Longitude: ' + response.data.ipLocation.longitude;
-            document.getElementById('region').innerHTML = 'Região: ' + response.data.ipLocation.region;
+            if(response.data.hasOwnProperty('ipLocation')){
+                document.getElementById('city').innerHTML = response.data.ipLocation.hasOwnProperty('city') ? 'Cidade: ' +  response.data.ipLocation.city : 'Não foi possível encontrar a cidade';
+                document.getElementById('country').innerHTML = response.data.ipLocation.hasOwnProperty('country') ? 'País: ' +  response.data.ipLocation.country : 'Não foi possível encontrar o País';
+                document.getElementById('countrycode').innerHTML = response.data.ipLocation.hasOwnProperty('country_code') ? 'Código do país: ' + response.data.ipLocation.country_code : 'Não foi possível encontrar o Código do país';
+                document.getElementById('latitude').innerHTML =  response.data.ipLocation.hasOwnProperty('latitude') ? 'Latitude: ' + response.data.ipLocation.latitude : 'Não foi possível encontrar a Latitude';
+                document.getElementById('longitude').innerHTML = response.data.ipLocation.hasOwnProperty('longitude') ? 'Longitude: ' +  response.data.ipLocation.longitude : 'Não foi possível encontrar o Longitude';
+                document.getElementById('region').innerHTML = response.data.ipLocation.hasOwnProperty('region') ? 'Região: ' +  response.data.ipLocation.region : 'Não foi possível encontrar a Região';
+            } else {
+                
+                document.getElementById('city').innerHTML = 'Não foi possível encontrar a cidade';
+                document.getElementById('country').innerHTML = 'Não foi possível encontrar o País';
+                document.getElementById('countrycode').innerHTML = 'Não foi possível encontrar o Código do país';
+                document.getElementById('latitude').innerHTML = 'Não foi possível encontrar a Latitude';
+                document.getElementById('longitude').innerHTML = 'Não foi possível encontrar o Longitude';
+                document.getElementById('region').innerHTML = 'Não foi possível encontrar a Região';
+            }
 
         //Clearout
-        document.getElementById('domain').innerHTML = 'Domínio: ' + response.data.clearout.detail_info.domain;
-        document.getElementById('free').innerHTML = 'Gratuito: ' + response.data.clearout.free;
-        document.getElementById('role').innerHTML = 'Perfil: ' + response.data.clearout.role;
-        document.getElementById('disposable').innerHTML = 'Temporário: ' + response.data.clearout.disposable;
-        document.getElementById('safe2send').innerHTML = 'Seguro para envio: ' + response.data.clearout.safe_to_send;
-        document.getElementById('verified').innerHTML = 'Verificado: ' + response.data.clearout.verified_on; */
+        if(response.data.hasOwnProperty('clearout')){
+            document.getElementById('domain').innerHTML = response.data.clearout.hasOwnProperty('detail_info') && response.data.clearout.detail_info.hasOwnProperty('domain') ? 'Domínio: ' + response.data.clearout.detail_info.domain : 'Não foi possível encontrar o domínio';
+            document.getElementById('free').innerHTML = response.data.clearout.hasOwnProperty('free') ? 'Gratuito: ' + response.data.clearout.free : 'Não foi possível verificar se é gratuito';
+            document.getElementById('role').innerHTML = response.data.clearout.hasOwnProperty('role') ? 'Perfil: ' + response.data.clearout.role : 'Não foi possível verificar se está em algum perfil';
+            document.getElementById('disposable').innerHTML = response.data.clearout.hasOwnProperty('disposable') ? 'Temporário: ' + response.data.clearout.disposable : 'Não foi possível verificar se é temporário';
+            document.getElementById('verified').innerHTML = response.data.clearout.hasOwnProperty('verified_on') ? 'Verificado: ' + response.data.clearout.verified_on : 'Não existe data de verificação';
+        } else {
+            document.getElementById('domain').innerHTML = 'Não foi possível encontrar o domínio';
+            document.getElementById('free').innerHTML = 'Não foi possível verificar se é gratuito';
+            document.getElementById('role').innerHTML = 'Não foi possível verificar se está em algum perfil';
+            document.getElementById('disposable').innerHTML = 'Não foi possível verificar se é temporário';
+            document.getElementById('verified').innerHTML = 'Não existe data de verificação';
+        }
 
         //Deu certo o PhishTank?
         let phisingTank = document.querySelectorAll('*[id^="phish"]')
         phisingTank.forEach((item) => item.remove())
         response.data.phish.map((item, index) => {
-            document.getElementById(`firstTdP`).insertAdjacentHTML(`afterend`, `<tr><td colspan='2' id="phish${index}u"><b>URL: ${item.url}</b></td></tr><tr><td>Foi possível realizar a checagem?</td><td id="phish${index}d">${item.database ? 'Sim' : 'Não'}</td></tr><tr><td id="phish${index}va"> É válido? </td><td>${item.valid}</td></tr><tr><td id="phish${index}ve"> Foi verificado?</td><td> ${item.verified}</td></tr><tr><td id="phish${index}at> Verificado quando?</td><td> ${item.verified_at}</td></tr>`);
+            document.getElementById(`firstTdP`).insertAdjacentHTML(`beforeend`, `<br><table  class="alt"><thead><tr><th>Descrição</th><th>Resultado</th></tr><thead><tbody></tr><tr><td colspan='2' id="phish${index}u"><b>URL: ${item.url}</b></td></tr><tr><td>Foi possível realizar a checagem?</td><td id="phish${index}d">${item.database ? 'Sim' : 'Não'}</td></tr><tr><td id="phish${index}va"> É válido? </td><td>${item.valid}</td></tr><tr><td id="phish${index}ve">Foi verificado?</td><td> ${item.verified}</td></tr><tr><td id="phish${index}at>Verificado quando?</td><td> ${item.verified_at}</td></tr></tbody></table>`);
         })
 
     })
